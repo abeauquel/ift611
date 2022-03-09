579b8dc3f7d4b748003a8878e6b43a57fd36bc56
Sebastien O'Neel <sebastien.oneel@tutanota.com>
2022-03-09 09:03:11 -0500

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <chrono>
 #include <QtWidgets/QtWidgets>
-#include "src/frontend/labelclic.h"
+#include "src/frontend/window.h"
 
 int main() {
 
@@ -14,15 +14,22 @@ int main() {
     auto duration = std::chrono::seconds(1);
     int i = 0;
     QApplication app(i, nullptr);
+    Window window;
+    window.show();
+/*
     QGraphicsView view;
     view.setWindowTitle("Task Manager");
     view.resize(800, 700);
+*/
+/*
     QGraphicsScene *scene = new QGraphicsScene(&view);
+    scene->setBackgroundBrush(QBrush(QPixmap(":/grid/grid10.png")));
     view.setScene(scene);
-    QGraphicsRectItem *rect =new QGraphicsRectItem(0, 0, 120, 50);
-    scene->addItem(rect);
-    QGraphicsTextItem *text = scene->addText("CPU"); 
-    view.show();
+    //QGraphicsRectItem *rect =new QGraphicsRectItem(0, 0, 120, 50);
+    //scene->addItem(rect);
+    //QGraphicsTextItem *text = scene->addText("CPU"); 
+*/
+ //   view.show();
 
     return app.exec();
 }
